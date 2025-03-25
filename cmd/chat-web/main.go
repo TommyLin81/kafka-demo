0b233b27be6a2e8c5fa23b809b3c3e724803ad42
Tommy Lin <aaqqwwssxx77@gmail.com>
2025-03-25 22:39:04 +0800

    feat(deploy): dockerize chat-web and update kafka config

--- cmd/chat-web/main.go
+++ cmd/chat-web/main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("public"))
-	http.Handle("/", fs)
+	wsServer := os.Getenv("WEBSOCKET_SERVER")
+	if wsServer == "" {
+		wsServer = "localhost:80"
+	}
 
-	log.Println("chat web running on http://localhost:12346 ...")
-	if err := http.ListenAndServe(":12346", nil); err != nil {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.FileServer(http.Dir("public")).ServeHTTP(w, r)
+			return
+		}
+
+		tmpl, err := template.ParseFiles("public/index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		data := struct {
+			WSServer string
+		}{
+			WSServer: wsServer,
+		}
+
+		tmpl.Execute(w, data)
+	})
+
+	log.Println("chat web running on port:8080 ...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
